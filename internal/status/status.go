4cb6ef30244dfcc2027b4f86d12b5caf5d728721
gosha <goshatraksel@yandex.ru>
2023-09-10 18:37:17 +0300

    update status output

--- internal/status/status.go
+++ internal/status/status.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 	"os"
+	"text/tabwriter"
 	"time"
 
 	"github.com/traksel/goblaq/internal/watch"
@@ -25,21 +26,53 @@ func (s *Status) update(status int, currentTime string) *Status {
 
 func (s *Status) Get(name string) error {
 	var d watch.Data
-	file, err := d.Get(name)
-	if err == nil {
-		resp, _ := http.Get(fmt.Sprintf("%s://%s%s", file.Schema, file.Target, file.Path))
-		currentTime := time.Now().Format("Mon Jan _2 15:04:05 MST 2006")
-		fmt.Printf(
-			"NAME\tSTATUS\tTIMESTAMP\n%s\t%d\t%v",
-			file.Name,
-			resp.StatusCode,
-			currentTime,
-		)
-		writeData, _ := yaml.Marshal(s.update(resp.StatusCode, currentTime))
-		writeData2, _ := yaml.Marshal(file)
-
-		writeData = append(writeData, writeData2...)
-		os.WriteFile(fmt.Sprintf("%s/.goblaq/%s/data.yaml", home, name), writeData, os.ModePerm)
+	w := new(tabwriter.Writer)
+	w.Init(os.Stdout, 8, 8, 0, '\t', 0)
+	defer w.Flush()
+	if name == "all" {
+		names, _ := os.ReadDir(fmt.Sprintf("%s/.goblaq/", home))
+		for id, n := range names {
+			file, err := d.Get(n.Name())
+			if err == nil {
+				resp, _ := http.Get(fmt.Sprintf("%s://%s%s", file.Schema, file.Target, file.Path))
+				currentTime := time.Now().Format("Mon Jan _2 15:04:05 MST 2006")
+				if id == 0 {
+					fmt.Fprintf(
+						w,
+						"%s\t%s\t%s\n",
+						"NAME",
+						"STATUS",
+						"TIMESTAMP",
+					)
+				}
+				fmt.Fprintf(w,
+					"%s\t%d\t%s\n",
+					file.Name,
+					resp.StatusCode,
+					currentTime,
+				)
+				writeData, _ := yaml.Marshal(file)
+				writeData2, _ := yaml.Marshal(s.update(resp.StatusCode, currentTime))
+				writeData = append(writeData, writeData2...)
+				os.WriteFile(fmt.Sprintf("%s/.goblaq/%s/data.yaml", home, name), writeData, os.ModePerm)
+			}
+		}
+	} else {
+		file, err := d.Get(name)
+		if err == nil {
+			resp, _ := http.Get(fmt.Sprintf("%s://%s%s", file.Schema, file.Target, file.Path))
+			currentTime := time.Now().Format("Mon Jan _2 15:04:05 MST 2006")
+			fmt.Printf(
+				"NAME\tSTATUS\tTIMESTAMP\n%s\t%d\t%v",
+				file.Name,
+				resp.StatusCode,
+				currentTime,
+			)
+			writeData, _ := yaml.Marshal(file)
+			writeData2, _ := yaml.Marshal(s.update(resp.StatusCode, currentTime))
+			writeData = append(writeData, writeData2...)
+			os.WriteFile(fmt.Sprintf("%s/.goblaq/%s/data.yaml", home, name), writeData, os.ModePerm)
+		}
 	}
 	return nil
 }
